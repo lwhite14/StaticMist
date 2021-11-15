764819fd9bc8f84dc70fbf7b3be1f5ef3a213253
lwhite14 <55700734+lwhite14@users.noreply.github.com>
2021-11-15 21:21:36 +0000

    fix: viewmodel in the wrong position

--- Assets/Scripts/ViewmodelModelAttach.cs
+++ Assets/Scripts/ViewmodelModelAttach.cs
@@ -12,6 +12,9 @@ public class ViewmodelModelAttach : MonoBehaviour
 
     void Update()
     {
+        //transform.position = Vector3.Lerp(transform.position, target.position, smoothing * Time.deltaTime);
+        //transform.rotation = Quaternion.Lerp(transform.rotation, target.rotation, smoothing * Time.deltaTime);
+        //LerpTransform(transform, target, smoothing * Time.deltaTime);  
         transform.position = Vector3.Slerp(transform.position, targetPositional.position, smoothing * Time.deltaTime);
         transform.rotation = Quaternion.Slerp(transform.rotation, targetRotational.rotation, smoothing * Time.deltaTime);
     }
@@ -25,6 +28,13 @@ public class ViewmodelModelAttach : MonoBehaviour
     {
         targetPositional = newPosTarget;
         targetRotational = newRotTarget;
+    }
+
+    void LerpTransform(Transform t1, Transform t2, float t)
+    {
+        t1.position = Vector3.Slerp(t1.position, t2.position, t);
+        t1.rotation = Quaternion.Slerp(t1.rotation, t2.rotation, t);
+        t1.localScale = Vector3.Slerp(t1.localScale, t2.localScale, t);
     }
 }
 
