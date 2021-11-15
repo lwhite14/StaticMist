a270dd07d6935f72396dfecfc3c00ba6f80df960
lwhite14 <55700734+lwhite14@users.noreply.github.com>
2021-11-15 21:45:07 +0000

    feat: basic sensitivity slider

--- Assets/Scripts/UI/ControlsTab.cs
+++ Assets/Scripts/UI/ControlsTab.cs
@@ -1,10 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ControlsTab : MonoBehaviour
 {
     public GameObject controlsTab;
+    public Slider sensSlider;
+    public float sens = 5;
+    public MouseLook mouseLook;
+
+    void Start()
+    {
+        sensSlider.value = sens;
+    }
 
     void Update()
     {
@@ -19,6 +28,37 @@ public class ControlsTab : MonoBehaviour
                 controlsTab.SetActive(true);
             }
         }
+
+        if (Input.GetKeyDown(KeyCode.LeftBracket)) 
+        {
+            SensitivityDown();
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightBracket))
+        {
+            SensitivityUp();
+        }
     }
 
+    void SensitivityDown()
+    {
+        sens--;
+        if (sens < 0)
+        {
+            sens = 0;
+        }
+        sensSlider.value = sens;
+        mouseLook.SetMouseSensitivity((sens + 1) * 50);
+    }
+
+    void SensitivityUp() 
+    {
+        sens++;
+        if (sens > 5) 
+        {
+            sens = 5;
+        }
+        sensSlider.value = sens;
+        mouseLook.SetMouseSensitivity((sens + 1) * 50);
+    }
 }
