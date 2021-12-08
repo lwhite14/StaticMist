8ab027a794af0dadcbf3b8b3e23479e2b1b8968b
lwhite14 <55700734+lwhite14@users.noreply.github.com>
2021-12-08 03:32:18 +0000

    test: basic unit testing setup for play mode

---
+++ Assets/Tests/PlayMode/PlayerMovementTests.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+
+
+public class PlayerMovementTests
+{
+    [SetUp]
+    public void SetUp() 
+    {
+        
+    }
+
+    [UnityTest]
+    public IEnumerator Player_NewScene_PlayerExists()
+    {
+        
+        yield return null;
+    }
+
+    [UnityTest]
+    public IEnumerator MoveForward_MovementKeys_ChangeXPosition()
+    {
+
+        yield return null;
+    }
+
+    public IEnumerator MoveLaterally_MovementKeys_ChangeZPosition()
+    {
+
+        yield return null;
+    }
+
+    [UnityTest]
+    public IEnumerator CheckGrounded_OnFloor_ReturnsTrue()
+    {
+
+        yield return null;
+    }
+
+    [UnityTest]
+    public IEnumerator CheckGrounded_Jumped_ReturnsFalse()
+    {
+
+        yield return null;
+    }
+
+    [UnityTest]
+    public IEnumerator Jump_SpaceKey_ChangeYPosition()
+    {
+
+        yield return null;
+    }
+
+
+}
