26b42437d17ce17069c0831974e0118367867ff1
lwhite14 <55700734+lwhite14@users.noreply.github.com>
2022-02-03 22:34:41 +0000

    fix: chase music turning off when one monster stops

--- Assets/Scripts/Audio/MusicManager.cs
+++ Assets/Scripts/Audio/MusicManager.cs
@@ -9,24 +9,35 @@ public class MusicManager : MonoBehaviour
     public AudioClip goalMusic;
     AudioSource audioSource;
 
+    int chaseCounter = 0;
+
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
-        SwitchToTense();
+        audioSource.clip = tenseMusic;
+        audioSource.Play();
     }
 
     public void SwitchToTense() 
     {
-        audioSource.clip = tenseMusic;
-        audioSource.loop = true;
-        audioSource.Play(); 
+        chaseCounter--;
+        if (chaseCounter == 0)
+        {
+            audioSource.clip = tenseMusic;
+            audioSource.loop = true;
+            audioSource.Play();
+        }
     }
 
     public void SwitchToChase()
     {
-        audioSource.clip = chaseMusic;
-        audioSource.loop = true;
-        audioSource.Play();
+        chaseCounter++;
+        if (chaseCounter == 1)
+        {
+            audioSource.clip = chaseMusic;
+            audioSource.loop = true;
+            audioSource.Play();
+        }
     }
 
     public void SwitchToGoal() 
