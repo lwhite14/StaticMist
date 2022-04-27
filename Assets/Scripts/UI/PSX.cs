7c9e2b63910c7cafb6fc683ebaf9b853828cf23f
lwhite14 <55700734+lwhite14@users.noreply.github.com>
2022-04-27 16:44:04 +0100

    fix: viewmodel appearing over UI
    When not in Retro TV mode. And also fixed text crawl not appearing.

--- Assets/Scripts/UI/PSX.cs
+++ Assets/Scripts/UI/PSX.cs
@@ -12,6 +12,7 @@ public class PSX : MonoBehaviour
         if (isOn)
         {
             GameObject.Find("Main Camera").GetComponent<Camera>().targetTexture = targetTexture;
+            GameObject.Find("Canvas").GetComponent<Canvas>().renderMode = RenderMode.ScreenSpaceCamera;
             GameObject.Find("Canvas").GetComponent<Canvas>().planeDistance = 1.25f;
             GameObject.Find("Canvas").GetComponent<Canvas>().worldCamera = GameObject.Find("PSXCamera").GetComponent<Camera>();
             GameObject.Find("PostProcessingVolume").layer = 8;
@@ -19,6 +20,7 @@ public class PSX : MonoBehaviour
         else 
         {
             GameObject.Find("Main Camera").GetComponent<Camera>().targetTexture = null;
+            GameObject.Find("Canvas").GetComponent<Canvas>().renderMode = RenderMode.ScreenSpaceOverlay;
             GameObject.Find("Canvas").GetComponent<Canvas>().planeDistance = 0.05f;          
             GameObject.Find("Canvas").GetComponent<Canvas>().worldCamera = GameObject.Find("Main Camera").GetComponent<Camera>();
             GameObject.Find("PostProcessingVolume").layer = 7;      
